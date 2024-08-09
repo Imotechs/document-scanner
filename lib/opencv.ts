5b2f795549fa6053dee57d24f8a84e575cc00ee3
praveencbemca <praveencbemca@gmail.com>
2024-08-09 20:44:35 +0530

    opencv lib refactored with local js files

--- lib/opencv.ts
+++ lib/opencv.ts
@@ -7,7 +7,7 @@
 //             resolve();
 //             return;
 //           }
-  
+
 //           const script = document.createElement('script');
 //           script.id = scriptId;
 //           script.src = 'https://docs.opencv.org/4.x/opencv.js';
@@ -32,43 +32,44 @@
 
 // lib/opencv.ts
 export const loadOpenCV = async (): Promise<void> => {
-  if (typeof window !== 'undefined') {
-    if (!window.cv) {
-      return new Promise((resolve, reject) => {
-        const scriptId = 'opencv-js';
-        if (document.getElementById(scriptId)) {
-          const checkLoaded = () => {
-            if (window.cv && window.cv.imread) {
-              resolve();
-            } else {
-              setTimeout(checkLoaded, 50);
-            }
-          };
-          checkLoaded();
-          return;
-        }
+	if (typeof window !== 'undefined') {
+		if (!window.cv) {
+			return new Promise((resolve, reject) => {
+				const scriptId = 'opencv-js';
+				if (document.getElementById(scriptId)) {
+					const checkLoaded = () => {
+						if (window.cv && window.cv.imread) {
+							resolve();
+						} else {
+							setTimeout(checkLoaded, 50);
+						}
+					};
+					checkLoaded();
+					return;
+				}
 
-        const script = document.createElement('script');
-        script.id = scriptId;
-        script.src = 'https://docs.opencv.org/4.7.0/opencv.js';
-        script.async = true;
-        script.onload = () => {
-          const checkLoaded = () => {
-            if (window.cv && window.cv.imread) {
-              resolve();
-            } else {
-              setTimeout(checkLoaded, 50);
-            }
-          };
-          checkLoaded();
-        };
-        script.onerror = () => reject(new Error('Failed to load OpenCV.js'));
-        document.body.appendChild(script);
-      });
-    } else {
-      return Promise.resolve();
-    }
-  } else {
-    return Promise.reject(new Error('Window is undefined'));
-  }
+				const script = document.createElement('script');
+				script.id = scriptId;
+				// script.src = 'https://docs.opencv.org/4.7.0/opencv.js';
+				script.src = "/opencv-4.10.0.js"
+				script.async = true;
+				script.onload = () => {
+					const checkLoaded = () => {
+						if (window.cv && window.cv.imread) {
+							resolve();
+						} else {
+							setTimeout(checkLoaded, 50);
+						}
+					};
+					checkLoaded();
+				};
+				script.onerror = () => reject(new Error('Failed to load OpenCV.js'));
+				document.body.appendChild(script);
+			});
+		} else {
+			return Promise.resolve();
+		}
+	} else {
+		return Promise.reject(new Error('Window is undefined'));
+	}
 };
